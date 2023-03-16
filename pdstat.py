c5fb154f098b80347c9f0bd303cec7197f50703e
BertrandLo <bertrandlo@pdservice.com>
2023-03-17 07:06:00 +0800

    [feat] 增加由資料庫載入數據程序選項
    異動項目:
      - load_group_data

--- pdstat.py
+++ pdstat.py
@@ -1,6 +1,8 @@
-# -*- coding: utf-8 -*-
+from datetime import datetime
+import pandas as pd
 from pdstats.data_import import DataImporter, DataSeries
 from argparse import ArgumentParser
+from pdcomponent.data_entity import DataEntity
 
 
 def main(filename):
@@ -12,8 +14,48 @@ def main(filename):
             ds.report()
 
 
+def load_group_data(group_id, dt_begin, dt_end):
+    from pdcomponent.device import Device
+    dev = Device(group_id)
+    dt_begin = datetime.strptime(dt_begin, '%Y-%m-%dT%H:%M:%S')
+    dt_end = datetime.strptime(dt_end, '%Y-%m-%dT%H:%M:%S')
+    if ((dt_begin-dt_end).days > 125) or ((dt_begin-dt_end).days < -125):
+        raise RuntimeError("Invalid Date Range!")
+    print(group_id, dt_begin, dt_end)
+    dev.begin_datetime = dt_begin
+    dev.ending_datetime = dt_end
+    dev.load_trend_data()
+
+    for idx, ch in enumerate(dev.gChannel):
+        data = list()
+        pdm: DataEntity
+        data.append(["{}_CH{}".format(dev.gName, ch), "", ""])
+        data.append(["timestamp", "Magnitude", "Count"])
+        for row_count, pdm in enumerate(dev.trend_data.get(ch)):
+            data.append([pdm.bdTime, pdm.bdMV, pdm.bdCount])
+        df = pd.DataFrame(data)
+        ds = DataSeries(df, 0, "{}_CH{}".format(dev.gName, ch))
+        ds.report()
+
+
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("xlsx_filename", help="loading xlsx file exported by PDSimply")
+    group_file = parser.add_argument_group()
+    group_file.add_argument("--file", help="loading xlsx file exported by PDSimply")
+
+    group_loading = parser.add_argument_group()
+    group_loading.add_argument("--gno", type=int, help="loading data from PUMO database")
+    group_loading.add_argument("--begin", type=str, help="begin datetime, max range 125 days.")
+    group_loading.add_argument("--end", type=str, help="end datetime")
+
     args = parser.parse_args()
-    main(filename=args.xlsx_filename)
+
+    if args.gno is not None:
+        load_group_data(group_id=args.gno, dt_begin=args.begin, dt_end=args.end)
+        exit(0)
+
+    if args.file is not None:
+        main(filename=args.file)
+        exit(0)
+
+    parser.print_help()
