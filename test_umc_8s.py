56f86ee3a0b8083d1204108e31dad7155eef35b3
BertrandLo <bertrandlo@pdservice.com>
2024-06-27 13:30:06 +0800

    [test] 功能測試

---
+++ test_umc_8s.py
@@ -0,0 +1,23 @@
+import unittest
+
+from pdstat import evaluating_thresholding
+
+
+class TestUMC8SAnalysis(unittest.TestCase):
+
+    def test_evaluating(self):
+
+        from pdcomponent.customer import Customer
+
+        cu = Customer(55)
+        target_list = cu.get_all_devices()[:5]
+
+        ratio_list = [0.5, 1, 2]
+
+        dt_begin = "2024-01-01T00:00:00"
+        dt_end = "2024-05-31T23:59:59"
+
+        evaluating_mv = 5
+        evaluating_duration_minutes = [10, 15, 20, 25, 30]
+        evaluating_thresholding("Report", target_list, dt_begin, dt_end,
+                                evaluating_duration_minutes, evaluating_mv, ratio_list)
