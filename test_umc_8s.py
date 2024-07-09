78118b33f794d2aaacc259fa5a97255f96b30d4f
BertrandLo <bertrandlo@pdservice.com>
2024-07-09 14:56:26 +0800

    [test] 增加 evaluating_thresholding 測試

--- test_umc_8s.py
+++ test_umc_8s.py
@@ -21,3 +21,20 @@ class TestUMC8SAnalysis(unittest.TestCase):
         evaluating_duration_minutes = [10, 15, 20, 25, 30]
         evaluating_thresholding("Report", target_list, dt_begin, dt_end,
                                 evaluating_duration_minutes, evaluating_mv, ratio_list)
+
+    def test_discharge_evaluating(self):
+        from pdcomponent.device import Device
+
+        target_list = [Device(12348)]  # UMC-8S  HV-12M
+
+        ratio_list = [0.5, 1, 2]
+
+        dt_begin = "2020-01-01T00:00:00"
+        dt_end = "2020-05-31T23:59:59"
+
+        evaluating_mv = 0.5
+
+        evaluating_duration_minutes = [10, 15, 20, 25, 30]
+        evaluating_thresholding("Report", target_list, dt_begin, dt_end,
+                                evaluating_duration_minutes, evaluating_mv, ratio_list)
+
